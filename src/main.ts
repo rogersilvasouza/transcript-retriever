59a19ff8430e9b1441b0ed7ead5dd718e9e73316
Roger Souza <rogersilvasouza@hotmail.com>
2022-08-16 22:48:57 -0300

    feat: Lint and prettier

--- src/main.ts
+++ src/main.ts
@@ -3,51 +3,53 @@ import * as cheerio from 'cheerio';
 import _ from 'lodash';
 
 async function transcription(): Promise<void> {
-    console.time('get-data');
+  console.time('get-data');
 
-    const id = process.argv.slice(2);
+  const id = process.argv.slice(2);
 
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
 
-    await page.goto(
-        `https://www.youtube.com/watch?v=${id}`,
-        { waitUntil: 'networkidle0' }
-    );
+  await page.goto(`https://www.youtube.com/watch?v=${id}`, {
+    waitUntil: 'networkidle0',
+  });
 
-    const awaitOnButtonMoreActions: puppeteer.ElementHandle<Element>[] = await page.$$('button[aria-label="More actions"]');
+  const awaitOnButtonMoreActions: puppeteer.ElementHandle<Element>[] =
+    await page.$$('button[aria-label="More actions"]');
 
-    for (const buttonMoreActions of awaitOnButtonMoreActions) {
-        await buttonMoreActions.evaluate((i: HTMLElement) => {
-            i.click()
-        });
-    }
-
-    const items = await page.$$('#items > ytd-menu-service-item-renderer > tp-yt-paper-item')
-
-    for (const item of items) {
-        await item.evaluate((i: HTMLElement) => {
-            i.click()
-        });
-    }
-
-    await page.waitForSelector('#segments-container > ytd-transcript-segment-renderer')
-
-    const $ = cheerio.load(
-        await page.content()
-    );
-
-    $('.style-scope ytd-transcript-segment-renderer > div').each((i, row) => {
-        console.log(
-            i,
-            _.trim($(row).find('.segment-timestamp').text()),
-            _.trim($(row).find('.segment-text').text())
-        );
+  for (const buttonMoreActions of awaitOnButtonMoreActions) {
+    await buttonMoreActions.evaluate((i: HTMLElement) => {
+      i.click();
     });
+  }
 
-    await browser.close();
+  const items = await page.$$(
+    '#items > ytd-menu-service-item-renderer > tp-yt-paper-item',
+  );
 
-    console.timeEnd('get-data');
+  for (const item of items) {
+    await item.evaluate((i: HTMLElement) => {
+      i.click();
+    });
+  }
+
+  await page.waitForSelector(
+    '#segments-container > ytd-transcript-segment-renderer',
+  );
+
+  const $ = cheerio.load(await page.content());
+
+  $('.style-scope ytd-transcript-segment-renderer > div').each((i, row) => {
+    console.log(
+      i,
+      _.trim($(row).find('.segment-timestamp').text()),
+      _.trim($(row).find('.segment-text').text()),
+    );
+  });
+
+  await browser.close();
+
+  console.timeEnd('get-data');
 }
 
 transcription();
